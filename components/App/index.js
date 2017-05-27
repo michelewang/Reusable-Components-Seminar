a030502a0321b5c79143ca2bb4fb0b8786dfdcdf
Josh Seides <jseides@college.harvard.edu>
2017-05-27 11:02:42 -0400

    finished app.js (#4)
    * finished app.js
    
    * finished app.js
    1
    
    * finished app.js
    2
    
    * finished app.js
    2
    
    * finished app.js
    3
    
    * fixes

---
+++ components/App/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { StyleSheet, View } from 'react-native'
+import PropTypes from 'prop-types'
+
+import Board from '../Board'
+import Button from '../Button'
+
+const BOARDS = [{name: 'Bootcamp'}];
+const USERS = {name: 'Admin'};
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            boards: BOARDS,
+            users: USERS,
+            boardInput: ''
+        }
+    }
+
+    createBoard() {
+        this.setState(s => ({boards: [...s.boards, {name: s.boardInput}]}));
+    }
+
+    render() {
+        return (
+            <View style={styles.view}>
+                <TextInput style={styles.inputs}
+                           onChangeText={(boardInput) => {
+                               this.setState({boardInput})
+                           }}
+                           value={this.state.boardInput} />
+                <Button onClick={this.createBoard} text="Create Board" />
+
+                <View>{this.state.boards.map(b, i) => <Board key={i} {...b} />}</View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    inputs: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1
+    },
+    view: {
+        flex: 1
+    }
+});
