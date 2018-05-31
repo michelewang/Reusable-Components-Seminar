8906f8b25eb64a005b6923df34cdf40e8b1ac0b5
Michele Wang <mwang@Michele-Wangs-MacBook-Pro.local>
2018-05-31 11:54:28 -0400

    Added chat card to index

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-export {default as BaseForm} from './BaseForm'
-export {default as FormItem} from './FormItem'
-export {default as RandItemForm} from './RandItemForm'
-export {default as FinalForm} from './FinalForm'
+export { default as BaseForm } from "./BaseForm";
+export { default as FormItem } from "./FormItem";
+export { default as RandItemForm } from "./RandItemForm";
+export { default as FinalForm } from "./FinalForm";
+export { default as ChatCard } from "./ChatCard";
